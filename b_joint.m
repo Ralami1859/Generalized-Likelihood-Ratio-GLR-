f7277b9e79bd966d41d8a11a687622de6264f0da
Reda ALAMI <37814001+Ralami1859@users.noreply.github.com>
2019-07-07 23:47:52 +0200

    Delete b_joint.m

--- b_joint.m
+++
@@ -1,7 +0,0 @@
-function res = b_joint(t0,s,t,delta, sigma)
-    
-    a = 1./(s-t0+1)+1./(t-s);
-    b = 1+1./(t-t0+1);
-    c = 2.*(t-t0).*sqrt(t-t0+2)./delta;
-    res = sigma.*sqrt(2.*a.*b.*log(c));
-end    

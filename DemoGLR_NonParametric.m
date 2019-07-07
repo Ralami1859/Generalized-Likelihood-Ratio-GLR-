44b7a419c53d580f9ae49b5fc51895aee7d1dbca
Reda ALAMI <37814001+Ralami1859@users.noreply.github.com>
2019-07-07 23:48:51 +0200

    Add files via upload

---
+++ DemoGLR_NonParametric.m
@@ -0,0 +1,22 @@
+
+%--------------------------------------------------------
+%%                     Define the environment
+%------------------------------------------------------
+theta1 = 0.9; theta2 = 0.4;
+T = 200;
+environment = [rand(1,T/2) < theta1 rand(1,T/2) < theta2];
+delta = 0.1;
+sigma = 0.5;
+
+%--------------------------------------------------------
+%%                   Launch the GLR non-parametric
+%--------------------------------------------------------
+CPEstimations = GLR_NonParametric(environment,sigma,delta);
+
+
+%----------------------------------------------------------
+%%                Plotting the results
+%----------------------------------------------------------
+figure; plot(CPEstimations,'r.')
+xlabel('Time step')
+ylabel('Change-point Estimations')
--- GLRConfidenceLevel.m
+++ GLRConfidenceLevel.m
@@ -1,6 +1,9 @@
-function res = GLRConfidenceLevel(t0,s,t,delta, sigma,N)
-    a = 1./sum(N(t0:s))+1./sum(N(s+1:t));
-    b = 1+1./sum(N(t0:t));
-    c = 2.*(sum(N(t0:t-1))).*sqrt(sum(N(t0:t))+1)./delta;
+function res = GLRConfidenceLevel(t0,s,t,delta, sigma)
+    %a = 1./sum(N(t0:s))+1./sum(N(s+1:t));
+    a = 1./(s-t0+1)+1./(t-s);
+    %b = 1+1./sum(N(t0:t));
+    b = 1+1./(t-t0+1);
+    %c = 2.*(sum(N(t0:t-1))).*sqrt(sum(N(t0:t))+1)./delta;
+    c = 2.*(t-t0).*sqrt(t-t0+1+1)./delta;
     res = sigma.*sqrt(2.*a.*b.*log(c));
 end 

2040182f46d1e3be7511a9256e99a4511d51581f
Reda ALAMI <37814001+Ralami1859@users.noreply.github.com>
2019-07-07 23:47:13 +0200

    Delete GLRConfidence.m

--- GLRConfidence.m
+++
@@ -1,6 +0,0 @@
-function res = GLRConfidence(t0,s,t,delta, sigma,N)
-    a = 1./sum(N(t0:s))+1./sum(N(s+1:t));
-    b = 1+1./sum(N(t0:t));
-    c = 2.*(sum(N(t0:t-1))).*sqrt(sum(N(t0:t))+1)./delta;
-    res = sigma.*sqrt(2.*a.*b.*log(c));
-end 
